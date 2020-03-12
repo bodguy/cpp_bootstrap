35fe2176ad34ed69a8173a27ff4b6c6c67868399
alsrudgns99 <alsrudgns99@zuminternet.com>
2020-03-12 16:06:48 +0900

    add string header

--- src/shader.hpp
+++ src/shader.hpp
@@ -1,4 +1,5 @@
 #include <GL/glew.h>
+#include <string>
 
 bool loadFile(const std::string& filepath, std::string& out_source);
 unsigned int loadShaderFromFile(const std::string& vs_name, const std::string& fs_name);
