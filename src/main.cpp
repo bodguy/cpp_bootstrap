37f9e98c70ec0f82808f056c1ebfe2911e0d32cd
bodguy <alsrudgns99@gmail.com>
2020-03-13 13:02:19 +0900

    fix some issue

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <iostream>
 #include "file_reader.h"
 #include "stub.h"
@@ -11,8 +14,11 @@ int main() {
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-  GLFWwindow* window = glfwCreateWindow(500, 500, "LearnOpenGL", NULL, NULL);
-  if (window == NULL) {
+  int window_width = 500;
+  int window_height = 500;
+
+  GLFWwindow* window = glfwCreateWindow(window_width, window_height, "OpenGL Bootstrap", nullptr, nullptr);
+  if (window == nullptr) {
     std::cout << "Failed to create GLFW window" << std::endl;
     glfwTerminate();
     return -1;
@@ -21,7 +27,8 @@ int main() {
   glfwMakeContextCurrent(window);
 
   glewExperimental = GL_TRUE;
-  if (glewInit() != GLEW_OK) return -1;
+  if (glewInit() != GLEW_OK)
+    return -1;
   glEnable(GL_DEPTH_TEST);
 
   unsigned int cubeVAO, cubeVBO;
@@ -34,6 +41,10 @@ int main() {
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
   unsigned int shaderProgram = read_shader("../../shader/vs.glsl", "../../shader/fs.glsl");
 
+  glm::mat4 model = glm::mat4(1.0f);
+  glm::mat4 view = glm::lookAt(glm::vec3(0.0f, 0.0f, 3.0f), glm::vec3(0.0f, 0.0f, 3.0f) + glm::vec3(0.0f, 0.0f, -1.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+  glm::mat4 projection = glm::perspective(glm::radians(45.0f), static_cast<float>(window_width) / static_cast<float>(window_height), 0.1f, 100.0f);
+
   float deltaTime = 0.0f;
   float lastFrame = 0.0f;
 
@@ -41,14 +52,23 @@ int main() {
     float currentFrame = glfwGetTime();
     deltaTime = currentFrame - lastFrame;
     lastFrame = currentFrame;
-    std::cout << deltaTime << '\n';
+
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+      glfwSetWindowShouldClose(window, true);
+    }
 
     glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+    model = glm::rotate(model, deltaTime, glm::vec3(0.0f, 1.0f, 0.0f));
+
     glUseProgram(shaderProgram);
+    glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(model));
+    glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "view"), 1, GL_FALSE, glm::value_ptr(view));
+    glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
     glBindVertexArray(cubeVAO);
     glDrawArrays(GL_TRIANGLES, 0, 36);
+    glUseProgram(NULL);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
